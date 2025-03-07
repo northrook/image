0f381c55f03f79d505b3b3583353ff25497603f5
Martin <hello@northrook.com>
2025-03-07 13:16:24 +0000

    mend

--- src/Image.php
+++ src/Image.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace Support;
 
+use Intervention\Image\Encoders\{JpegEncoder, PngEncoder, WebpEncoder};
 use Intervention\Image\ImageManager;
 
 use Intervention\Image\Drivers\{Gd, Imagick};
+use Intervention\Image\Interfaces\ImageInterface;
+use SplFileInfo;
+use Stringable;
 
 final class Image
 {
@@ -21,6 +25,42 @@ final class Image
     public function __construct()
     {
         $this->driver = $this->getDriver();
+    }
+
+    public static function pngEncoder() : PngEncoder
+    {
+        return Image::getDriver() === 'imagick'
+                ? new Imagick\Encoders\PngEncoder()
+                : new Gd\Encoders\PngEncoder();
+    }
+
+    public static function jpegEncoder() : JpegEncoder
+    {
+        return Image::getDriver() === 'imagick'
+                ? new Imagick\Encoders\JpegEncoder()
+                : new Gd\Encoders\JpegEncoder();
+    }
+
+    public static function webpEncoder() : WebpEncoder
+    {
+        return Image::getDriver() === 'imagick'
+                ? new Imagick\Encoders\WebpEncoder()
+                : new Gd\Encoders\WebpEncoder();
+    }
+
+
+
+    public static function from( string|SplFileInfo|Stringable|ImageInterface $input ) : ImageInterface
+    {
+        if ( $input instanceof Stringable ) {
+            $input = $input->__toString();
+        }
+        return Image::getImageProcessor()->read( $input );
+    }
+
+    public static function create( int $width, int $height ) : ImageInterface
+    {
+        return Image::getImageProcessor()->create( $width, $height );
     }
 
     /**
